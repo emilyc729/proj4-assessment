482155eb93f89722857cdc6d1fb039eafb525bd1
Emily <echeun17@mail.ccsf.edu>
2019-10-07 14:12:01 -0700

    Updates

---
+++ precache-manifest.07ed6e9c8da3f7c41329816897205178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dabac7ec213209fa4745fb98a5f5667",
+    "url": "/proj4-assessment/index.html"
+  },
+  {
+    "revision": "ee0f3ad81dc9611cdbb2",
+    "url": "/proj4-assessment/static/css/main.2fc486e6.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/proj4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "ee0f3ad81dc9611cdbb2",
+    "url": "/proj4-assessment/static/js/main.85b8e180.chunk.js"
+  },
+  {
+    "revision": "6bbd96d276ca652f469d",
+    "url": "/proj4-assessment/static/js/runtime-main.35d123bb.js"
+  }
+]);
